75cb1dd5d03493cea67fc8ab486346b22574aaf6
Cyrus Goh <hello@lovincyrus.com>
2020-07-09 12:16:02 -0700

    install stylus and stylus-loader

--- pages/index.vue
+++ pages/index.vue
@@ -31,35 +31,31 @@
 export default {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="stylus" scoped>
+.container
+  margin 0 auto
+  min-height 100vh
+  display flex
+  justify-content center
+  align-items center
+  text-align center
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.title
+  font-family 'Inter', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif
+  display block
+  font-weight 300
+  font-size 100px
+  color #35495e
+  letter-spacing 1px
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+.subtitle
+  font-weight 300
+  font-size 42px
+  color #526488
+  word-spacing 5px
+  padding-bottom 15px
 
-.links {
-  padding-top: 15px;
-}
+.links
+  padding-top 15px
 </style>
