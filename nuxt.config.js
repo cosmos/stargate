26102913950c02454e5c11c61dfa03f543fa71b8
Cyrus Goh <hello@lovincyrus.com>
2020-07-15 12:35:04 -0700

    add postcss autoprefixer

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,5 +64,22 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    // https://nuxtjs.org/faq/postcss-plugins/#recommended-method
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      // plugins: {
+      //   // Disable a plugin by passing false as value
+      //   'postcss-url': false,
+      //   'postcss-nested': {},
+      //   'postcss-responsive-type': {},
+      //   'postcss-hexrgba': {},
+      // },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {},
+      },
+    },
+  },
 }
