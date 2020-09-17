9a6baf6735d5b00d460efd3083d89d715351f6f3
Cyrus Goh <hello@lovincyrus.com>
2020-09-17 14:52:58 -0700

    section-nav

---
+++ components/SectionNav.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="headroom section-nav">
+    <nav class="nav">
+      <ul class="nav__list">
+        <li class="nav__list__item">
+          <a v-scroll-to="'#intro'">What is Stargate?</a>
+        </li>
+        <li class="nav__list__item">
+          <a v-scroll-to="'#features'">Features</a>
+        </li>
+        <li class="nav__list__item">
+          <a v-scroll-to="'#testnets'">Testnets</a>
+        </li>
+        <li class="nav__list__item">
+          <a v-scroll-to="'#roadmap'">Roadmap</a>
+        </li>
+        <li class="nav__list__item">
+          <a v-scroll-to="'#contributors'">Contributors</a>
+        </li>
+        <li class="nav__list__item">
+          <a v-scroll-to="'#articles'">Resources</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="stylus" scoped>
+.headroom
+  will-change transform
+  transition transform .2s ease-out
+
+.headroom--pinned
+  transform translateY(0%)
+
+.headroom--unpinned
+  transform translateY(-100%)
+
+.section-nav
+  top 0
+  width 100%
+  z-index 1000
+  &.headroom--pinned,
+  &.headroom--unpinned
+    position fixed
+    &:not(.headroom--frozen)
+      background linear-gradient(180deg, rgba(0,0,0,0.96) 41%, rgba(0, 0, 0, 0))
+      padding-bottom 1rem
+  &.headroom--frozen
+    position static
+  .nav
+    overflow scroll
+    white-space nowrap
+    font-size 1.1875rem
+    line-height 1.263
+    letter-spacing -0.005em
+    &__list
+      display flex
+      flex-direction row
+      justify-content center
+      list-style none
+      padding-left 4rem
+      padding-right 4rem
+      &__item
+        + .nav__list__item
+          margin-left 2rem
+        a
+          display inline-block
+          padding 1.25rem 0
+          position relative
+          z-index 1
+          color #989BB9
+          &:hover,
+          &:focus
+            color #ECEDF7
+          &:hover:before
+            visibility visible
+            transform scaleX(1)
+          &:before
+            content ""
+            position absolute
+            width 100%
+            height 2px
+            bottom 2px
+            z-index -1
+            border-radius 4px
+            background-color #3B2AB7
+            visibility hidden
+            transform scaleX(0)
+            transition all 300ms cubic-bezier(0.325, -0.075, 0, 1.65)
+
+@media screen and (max-width: 1024px)
+  // .section .section-header
+  //     grid-column 1 / span 12
+
+  .section-nav
+    .nav
+      &::-webkit-scrollbar
+        display none
+      &__list
+        display flex
+        flex-direction row
+        justify-content space-between
+        transition transform 200ms ease 0s
+        overflow-x scroll
+        overflow-y hidden
+        white-space nowrap
+        width fit-content
+        -webkit-overflow-scrolling touch
+
+@media screen and (max-width: 767px)
+  .section-nav
+    .nav
+      &__list
+        padding-left 1.5rem
+        padding-right 1.5rem
+
+@media screen and (max-width: 576px)
+  .section-nav
+    .nav
+      font-size 1rem
+      line-height 1.375
+      letter-spacing 0
+      &__list__item a
+        padding 0.8125rem 0
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,61 +9,13 @@
     </div>
 
     <div class="nav-container">
-      <header id="section-nav" class="headroom section-nav">
-        <nav class="nav">
-          <ul class="nav__list">
-            <li class="nav__list__item">
-              <a v-scroll-to="'#intro'">What is Stargate?</a>
-            </li>
-            <li class="nav__list__item">
-              <a v-scroll-to="'#features'">Features</a>
-            </li>
-            <li class="nav__list__item">
-              <a v-scroll-to="'#testnets'">Testnets</a>
-            </li>
-            <li class="nav__list__item">
-              <a v-scroll-to="'#roadmap'">Roadmap</a>
-            </li>
-            <li class="nav__list__item">
-              <a v-scroll-to="'#contributors'">Contributors</a>
-            </li>
-            <li class="nav__list__item">
-              <a v-scroll-to="'#articles'">Resources</a>
-            </li>
-          </ul>
-        </nav>
+      <header id="section-nav">
+        <section-nav />
       </header>
     </div>
 
-    <div id="intro" class="section section-intro">
-      <div class="tm-section-container section-container">
-        <div class="container">
-          <div class="section-title">A new era for Cosmos</div>
-          <div class="section-subtitle">
-            The biggest event in the ecosystem since the launch of the Cosmos
-            Hub.
-          </div>
-          <eclipsed-planet class="section-graphics" />
-          <div class="section-statement">
-            <p>
-              Stargate is a set of upgrades that complete the original roadmap
-              laid out in the Cosmos Whitepaper. For the first time ever, Cosmos
-              blockchains will be able to connect with each other using the
-              first standardized protocol for inter-blockchain communication
-              (IBC).
-            </p>
-            <div class="section-cta">
-              <a
-                href="https://blog.cosmos.network/cosmos-stargate-upgrade-overview-8939475fe673"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Read more about Stargate &#8594;
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div id="intro">
+      <section-intro />
     </div>
 
     <div
@@ -71,102 +23,8 @@
       v-observe-visibility="{
         callback: unfreezeNav,
       }"
-      class="section section-features"
     >
-      <div class="tm-section-container section-container">
-        <div class="container">
-          <div class="section-header">Features</div>
-          <div class="section-title">What’s new?</div>
-          <div class="section-statement">
-            These upgrades bring significant performance improvements to
-            blockchains built with Cosmos SDK. Discover all the amazing features
-            of the Stargate upgrade below.
-          </div>
-          <div class="data-container">
-            <div class="data-item">
-              <div class="data-item__overline">Enabling</div>
-              <div class="data-item__heading">IBC</div>
-            </div>
-            <div class="data-item">
-              <div class="data-item__overline">Efficiency gains up to</div>
-              <div class="data-item__heading">100x</div>
-            </div>
-            <div class="data-item">
-              <div class="data-item__overline">Faster full node sync</div>
-              <div class="data-item__heading">96x</div>
-            </div>
-            <div class="data-item">
-              <div class="data-item__overline">Faster upgrades</div>
-              <div class="data-item__heading">20x</div>
-            </div>
-          </div>
-          <div class="features-graphics">
-            <interoperable-planets />
-          </div>
-          <div class="ibc-item">
-            <div class="ibc-item__icon">
-              <icon-ibc-cropped />
-            </div>
-            <div class="ibc-item__title">Inter-Blockchain Communication</div>
-            <div class="ibc-item__description">
-              Cosmos SDK-based blockchains can incorporate the Inter-Blockchain
-              Communication (IBC) Protocol to communicate with other compatible
-              blockchains. The Cosmos Hub is the first blockchain to support IBC
-              in production.
-            </div>
-          </div>
-          <div class="grid-container">
-            <div class="grid-item">
-              <div class="grid-item__icon">
-                <icon-countdown />
-              </div>
-              <div class="grid-item__title">State Sync</div>
-              <div class="grid-item__description">
-                For blockchains built on top of Tendermint Core, the full-node
-                synchronization process will be reduced from 2 days to just 30
-                mins or less.
-              </div>
-            </div>
-            <div class="grid-item">
-              <div class="grid-item__icon">
-                <icon-repeat />
-              </div>
-              <div class="grid-item__title">Automatic upgrades</div>
-              <div class="grid-item__description">
-                The upgrade module facilitates the upgrade process of chains
-                built with Cosmos SDK. Migration from one version to the next
-                will take little to no time, as opposed to one hour or more
-                before.
-              </div>
-            </div>
-            <div class="grid-item">
-              <div class="grid-item__icon">
-                <icon-mask-oval />
-              </div>
-              <div class="grid-item__title">Full-featured Light Clients</div>
-              <div class="grid-item__description">
-                Light clients are lightweight alternatives to a full node,
-                allowing a wide range of devices to safely and efficiently
-                receive blockchain data for new network nodes and
-                inter-blockchain communication.
-              </div>
-            </div>
-            <div class="grid-item">
-              <div class="grid-item__icon">
-                <icon-speedometer />
-              </div>
-              <div class="grid-item__title">
-                Transition from Amino to Protobuf
-              </div>
-              <div class="grid-item__description">
-                Protobuf brings performance improvements and developer
-                acceleration to Cosmos SDK-based blockchains with efficiency
-                gains of 10-100x, and wider language support.
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <section-features />
     </div>
 
     <div id="testnets" class="section section-testnets">
@@ -1081,18 +939,21 @@ export default {
   background transparent !important
   color #989BB9 !important
 
+.nav-container
+  height 4rem
+
 main
   overflow -webkit-paged-x
 
-.headroom
-  will-change transform
-  transition transform .2s ease-out
+// .headroom
+//   will-change transform
+//   transition transform .2s ease-out
 
-.headroom--pinned
-  transform translateY(0%)
+// .headroom--pinned
+//   transform translateY(0%)
 
-.headroom--unpinned
-  transform translateY(-100%)
+// .headroom--unpinned
+//   transform translateY(-100%)
 
 .fade-enter-active
   transition all .4s ease-out
@@ -1116,189 +977,64 @@ main
   opacity 0
   transform scale(.96)
 
-.section
-  overflow hidden
-  position relative
-  padding-top 6rem
-  padding-bottom 6rem
-
-.section-header
-  padding-top 1rem
-  min-height 3rem
-  font-weight 600
-  font-size 1.1875rem
-  line-height 1.263
-  letter-spacing 0.08em
-  text-transform uppercase
-  color #989BB9
-  border-top 1px solid #282B53
-
-.section-intro
-  position relative
-  .section-title
-    grid-column 1 / span 12
-    grid-row 1 / 2
-    font-weight 900
-    font-size 100px
-    line-height 112%
-    letter-spacing -0.03em
-    color #FFFFFF
-    max-width 50rem
-  .section-subtitle
-    display flex
-    justify-content flex-end
-    flex-direction column
-    margin-top -1.5em
-    grid-row 2
-    grid-column 6 / span 7
-    font-size 2rem
-    line-height 1.25
-    letter-spacing -0.02em
-    color #FFFFFF
-  .section-graphics
-    grid-row 3
-    grid-column 1 / span 6
-    position relative
-  .section-statement
-    position relative
-    margin-top 3rem
-    grid-column 6 / span 7
-    grid-row 3
-    max-width 30em
-    font-size 1.4375rem
-    line-height 1.565
-    letter-spacing -0.01em
-    text-align left
-    p
-      color #CFD1E7
-  .section-cta
-    margin-top 3rem
-    a
-      display inline-block
-      font-weight bold
-      font-size 1.4375rem
-      line-height 1.217
-      letter-spacing -0.01em
-      border-radius 6px
-      padding 22px 0
-      &:hover
-        color inherit
-  .title
-    font-weight 900
-    font-size 100px
-    line-height 112%
-    display flex
-    align-items center
-    letter-spacing -0.03em
-    color var(--white)
-
-.section-features
-  position relative
-  text-align left
-  .section-header
-    grid-row 1
-    grid-column 6 / span 7
-  .section-title
-    grid-row 2
-    margin-top 3rem
-    margin-bottom 3rem
-    font-weight 900
-    font-size 5.125rem
-    line-height 1.121
-    letter-spacing -0.025em
-    color #FFFFFF
-    grid-column 6 / span 7
-    text-align left
-  .section-statement
-    grid-row 3
-    grid-column 6 / span 7
-    max-width 30em
-    font-size 1.4375rem
-    line-height 1.565
-    letter-spacing -0.01em
-    color #CFD1E7
-  .data-container
-    position relative
-    z-index 1 // above .feature-graphics
-    grid-row 4
-    margin-top 9rem
-    display grid
-    flex-direction row
-    grid-column 1/ span 12
-    grid-template-columns repeat(4, 1fr)
-    gap 2rem
-    .data-item
-      border-top 1px dashed #282B53
-      &__overline
-        margin-top 1rem
-        font-weight 600
-        font-size 16px
-        line-height 137.5%
-        letter-spacing 0.08em
-        text-transform uppercase
-        color #989BB9
-      &__heading
-        margin-top 16px
-        font-weight bold
-        font-size 58px
-        line-height 117.2%
-        letter-spacing -0.03em
-        color #CCD1FF
-  .features-graphics
-    grid-column 1 / span 8
-    grid-row 5
-    position relative
-    font-size 1.5rem
-    z-index 0
-  .ibc-item
-    position relative
-    grid-column 9 / span 4
-    grid-row 5
-    align-self center
-    max-width 24em
-    font-size 1rem
-    line-height 1.625
-    color #989BB9
-    &__icon
-      width 3em
-      height 3em
-    &__title
-      margin-top 1.5rem
-      font-weight bold
-      font-size 1.75rem
-      line-height 1.285
-      letter-spacing -0.02em
-      color #FFFFFF
-    &__description
-      margin-top 1rem
-  .grid-container
-    margin-top 21%
-    display grid
-    grid-template-columns repeat(9, 1fr)
-    gap 6rem 2rem
-    grid-column 4 / span 9
-    grid-row 6
-    color white
-    .grid-item
-      grid-column 1 / span 4
-      max-width 24em
-      font-size 1rem
-      line-height 1.625
-      color #989BB9
-      &:nth-child(even)
-        grid-column 6 / span 4
-      &__icon
-        width 3rem
-        height 3rem
-      &__title
-        margin-top 1.5rem
-        font-weight bold
-        font-size 1.75rem
-        line-height 1.285
-        letter-spacing -0.02em
-        color #FFFFFF
-      &__description
-        margin-top 1rem
+// .section-intro
+//   position relative
+//   .section-title
+//     grid-column 1 / span 12
+//     grid-row 1 / 2
+//     font-weight 900
+//     font-size 100px
+//     line-height 112%
+//     letter-spacing -0.03em
+//     color #FFFFFF
+//     max-width 50rem
+//   .section-subtitle
+//     display flex
+//     justify-content flex-end
+//     flex-direction column
+//     margin-top -1.5em
+//     grid-row 2
+//     grid-column 6 / span 7
+//     font-size 2rem
+//     line-height 1.25
+//     letter-spacing -0.02em
+//     color #FFFFFF
+//   .section-graphics
+//     grid-row 3
+//     grid-column 1 / span 6
+//     position relative
+//   .section-statement
+//     position relative
+//     margin-top 3rem
+//     grid-column 6 / span 7
+//     grid-row 3
+//     max-width 30em
+//     font-size 1.4375rem
+//     line-height 1.565
+//     letter-spacing -0.01em
+//     text-align left
+//     p
+//       color #CFD1E7
+//   .section-cta
+//     margin-top 3rem
+//     a
+//       display inline-block
+//       font-weight bold
+//       font-size 1.4375rem
+//       line-height 1.217
+//       letter-spacing -0.01em
+//       border-radius 6px
+//       padding 22px 0
+//       &:hover
+//         color inherit
+//   .title
+//     font-weight 900
+//     font-size 100px
+//     line-height 112%
+//     display flex
+//     align-items center
+//     letter-spacing -0.03em
+//     color var(--white)
 
 .section-testnets
   position relative
@@ -1535,62 +1271,6 @@ tr
 td
   &:nth-child(2)
     font-weight 600
-
-.nav-container
-  height 4rem
-
-.section-nav
-  top 0
-  width 100%
-  z-index 1000
-  &.headroom--pinned,
-  &.headroom--unpinned
-    position fixed
-    &:not(.headroom--frozen)
-      background linear-gradient(180deg, rgba(0,0,0,0.96) 41%, rgba(0, 0, 0, 0))
-      padding-bottom 1rem
-  &.headroom--frozen
-    position static
-  .nav
-    overflow scroll
-    white-space nowrap
-    font-size 1.1875rem
-    line-height 1.263
-    letter-spacing -0.005em
-    &__list
-      display flex
-      flex-direction row
-      justify-content center
-      list-style none
-      padding-left 4rem
-      padding-right 4rem
-      &__item
-        + .nav__list__item
-          margin-left 2rem
-        a
-          display inline-block
-          padding 1.25rem 0
-          position relative
-          z-index 1
-          color #989BB9
-          &:hover,
-          &:focus
-            color #ECEDF7
-          &:hover:before
-            visibility visible
-            transform scaleX(1)
-          &:before
-            content ""
-            position absolute
-            width 100%
-            height 2px
-            bottom 2px
-            z-index -1
-            border-radius 4px
-            background-color #3B2AB7
-            visibility hidden
-            transform scaleX(0)
-            transition all 300ms cubic-bezier(0.325, -0.075, 0, 1.65)
 
 .section-roadmap
   .section-header
@@ -2109,31 +1789,24 @@ td
       .text
         grid-column 8 / span 4
 
-@media screen and (max-width: 1279px)
-  .section-features
-    .features-graphics
-      font-size 1.25rem
-    .grid-container
-      grid-column 1/span 12
-
 @media screen and (max-width: 1024px)
   .section .section-header
       grid-column 1 / span 12
 
-  .section-nav
-    .nav
-      &::-webkit-scrollbar
-        display none
-      &__list
-        display flex
-        flex-direction row
-        justify-content space-between
-        transition transform 200ms ease 0s
-        overflow-x scroll
-        overflow-y hidden
-        white-space nowrap
-        width fit-content
-        -webkit-overflow-scrolling touch
+  // .section-nav
+  //   .nav
+  //     &::-webkit-scrollbar
+  //       display none
+  //     &__list
+  //       display flex
+  //       flex-direction row
+  //       justify-content space-between
+  //       transition transform 200ms ease 0s
+  //       overflow-x scroll
+  //       overflow-y hidden
+  //       white-space nowrap
+  //       width fit-content
+  //       -webkit-overflow-scrolling touch
 
   .section-intro
     .section-title
@@ -2150,23 +1823,6 @@ td
     .section-statement
       grid-row 4
       grid-column 1 / span 12
-
-  .section-features
-    .section-title
-      grid-column 1 / 12
-      font-size 4rem
-    .section-statement
-      grid-column 1 / 12
-    .data-container
-      grid-template-columns repeat(2, 1fr)
-    .features-graphics
-      grid-column 1 / span 10
-    .ibc-item
-      grid-row 6
-      grid-column 1 / 12
-      margin-top 2rem
-    .grid-container
-      grid-row 7
 
   .section-testnets
     .section-graphics
@@ -2246,29 +1902,17 @@ td
     grid-template-columns repeat(1, 1fr)
 
 @media screen and (max-width: 767px)
-  .section
-    .section-header
-      font-size 1rem
-      line-height 1.375
-
-  .section-container
-    padding-top 1.5rem
-
-  .section-nav
-    .nav
-      &__list
-        padding-left 1.5rem
-        padding-right 1.5rem
+  // .section-nav
+  //   .nav
+  //     &__list
+  //       padding-left 1.5rem
+  //       padding-right 1.5rem
 
   .section-intro
     .section-graphics
       font-size 0.75rem
     .section-statement
       margin-top 1rem
-
-  .section-features
-    .features-graphics
-      font-size 1rem
 
   .section-testnets
     .section-status
@@ -2308,13 +1952,13 @@ td
       line-height 1.375
       margin-top 1.5rem
 
-  .section-nav
-    .nav
-      font-size 1rem
-      line-height 1.375
-      letter-spacing 0
-      &__list__item a
-        padding 0.8125rem 0
+  // .section-nav
+  //   .nav
+  //     font-size 1rem
+  //     line-height 1.375
+  //     letter-spacing 0
+  //     &__list__item a
+  //       padding 0.8125rem 0
 
   .section-intro
     .section-graphics
@@ -2325,33 +1969,6 @@ td
       line-height 1.375
       letter-spacing 0
       padding 0.8125rem 0
-
-  .section-features
-    .data-container
-      margin-top 3rem
-      .data-item
-        &__overline
-          font-size 0.8125rem
-          line-height 1.384
-        &__heading
-          font-size 2.5rem
-          line-height 1.2
-    .features-graphics
-      font-size 0.5rem
-    .grid-container
-      margin-top 3rem
-      gap 3rem 2rem
-      .grid-item
-        &:nth-child(even),
-        &:nth-child(odd)
-          grid-column 1/span 12
-          grid-template-columns repeat(1, 1fr)
-
-    .ibc-item__title,
-    .grid-container .grid-item__title
-      font-size 1.4375rem
-      line-height 1.217
-      letter-spacing -0.01em
 
   .section-testnets
     .section-graphics
@@ -2524,17 +2141,7 @@ td
   .section-container .container
     gap unset
 
-  .section-features .grid-container .grid-item
-    max-width 20em
-
 @media screen and (max-width: 320px)
-  .section-features
-    .grid-container
-      .grid-item
-        &:nth-child(even),
-        &:nth-child(odd)
-          grid-column 1/span 9
-
   .section-articles
     .section-title
       max-width 17rem
