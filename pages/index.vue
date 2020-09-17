cf46c3325b2a48576e8b1cc23b2b340c2558806a
Cyrus Goh <hello@lovincyrus.com>
2020-09-17 14:17:31 -0700

    section-hero component

--- pages/index.vue
+++ pages/index.vue
@@ -4,53 +4,8 @@
       v-observe-visibility="{
         callback: freezeNav,
       }"
-      class="section-hero"
     >
-      <div class="tm-section-container section-container">
-        <div class="hero-graphics">
-          <span class="hero-graphics__gate"></span>
-          <gate-notches class="hero-graphics__gate-notches" />
-          <span class="hero-graphics__star"></span>
-          <span class="hero-graphics__planet"></span>
-        </div>
-        <div class="container">
-          <nav class="nav-primary">
-            <ul>
-              <li>
-                <a href="https://cosmos.network">
-                  <span class="text">← Cosmos Network</span>
-                  <icon-arrow-right class="icon" />
-                </a>
-              </li>
-              <li>
-                <logo-wordmark class="logo" /><span class="sr-only"
-                  >Cosmos</span
-                >
-              </li>
-            </ul>
-          </nav>
-          <div class="headings">
-            <h1 class="title">
-              <wordmark-stargate />
-              <span class="sr-only">Stargate</span>
-            </h1>
-            <div class="subtitle">
-              The Internet of Blockchains is on the horizon.
-            </div>
-          </div>
-          <div class="hero-bottom">
-            <tm-button
-              v-scroll-to="'#testnets'"
-              size="l"
-              color="#fff"
-              variant="contained"
-              glow
-              class="hero-btn"
-              >Join testnets</tm-button
-            >
-          </div>
-        </div>
-      </div>
+      <section-hero />
     </div>
 
     <div class="nav-container">
@@ -1161,30 +1116,11 @@ main
   opacity 0
   transform scale(.96)
 
-// Accessible/SEO friendly CSS hiding
-.sr-only
-  position absolute
-  height 1px
-  width 1px
-  overflow hidden
-  clip rect(1px, 1px, 1px, 1px)
-
 .section
   overflow hidden
   position relative
   padding-top 6rem
   padding-bottom 6rem
-
-.section-container
-  position relative
-  max-width 96rem
-  margin 0 auto
-  .container
-    display grid
-    grid-template-columns repeat(12, 1fr)
-    gap 0 2rem
-    max-width var(--container-mw-lg)
-    margin 0 auto
 
 .section-header
   padding-top 1rem
@@ -1196,177 +1132,6 @@ main
   text-transform uppercase
   color #989BB9
   border-top 1px solid #282B53
-
-.nav-primary
-  ul
-    display grid
-    grid-template-columns repeat(3, 1fr)
-    align-items center
-    padding 0
-  li
-    list-style-type none
-    &:first-child
-      text-align left
-  .logo
-    display block
-    margin 0 auto
-  a
-    display inline-block
-    font-size 1rem
-    line-height 1.375
-    padding 0.8125rem 0
-    color #989BB9
-    border-radius 0.375rem
-    &:hover,
-    &:focus
-      color #CFD1E7
-    .icon
-      display none
-      fill #CFD1E7
-      transform rotate(180deg)
-      width 1.5rem
-
-.section-hero
-  position relative
-  height 100vh
-  min-height 52rem
-  max-height 64rem
-  margin-bottom 6rem
-  display flex
-  justify-content stretch
-  align-items stretch
-  text-align center
-  background radial-gradient(60.92% 51.1% at 50% 57.31%, #21255F 26.56%, #030419 80.73%, #000000 100%)
-  .section-container
-    display flex
-    align-items stretch
-    width 100%
-    padding-top 4rem
-    padding-bottom 6rem
-  .container
-    position relative
-    display flex
-    width 100%
-    flex-direction column
-    align-items normal
-    justify-content space-between
-    .nav-primary
-      margin-bottom 5%
-    .headings
-      width 100%
-      .title
-        margin 0
-        svg
-          width 100%
-          height auto
-          max-height 5.5rem
-      .subtitle
-        margin-top 3rem
-        font-size 2rem
-        line-height 150%
-        letter-spacing -0.02em
-        color #989BB9
-
-.section-hero
-  .hero-graphics
-    position absolute
-    top 10%
-    left 0
-    bottom -100%
-    padding-top 50%
-    width 100%
-    overflow hidden
-    &__gate,
-    &__gate-notches
-      position absolute
-      width 78%
-      left 11%
-      top 9.3%
-      transform translate3d(0,0,0)
-    &__gate
-      padding-bottom 78%
-      border-radius 50%
-      box-shadow inset 0 0.1875em 0.0625em 0 rgba(255, 255, 255, 0.3)
-      border 1px solid rgba(255, 255, 255, 0.1)
-      box-sizing content-box
-      animation 3s gate infinite linear
-      &:after
-        content ""
-        position absolute
-        top 3.2%
-        left 3.2%
-        right 3.2%
-        bottom 3.2%
-        border-radius 50%
-        box-shadow inset 0 -0.1875em 0.0625em 0 rgba(255, 255, 255, 0.3)
-        border 1px solid rgba(255, 255, 255, 0.1)
-        animation 1.4s gate infinite linear reverse
-    &__gate-notches
-      height auto
-      animation 40s gate infinite linear
-    &__star
-      position absolute
-      width 42%
-      padding-bottom 42%
-      left 29%
-      top 22%
-      border-radius 50%
-      background #FFFFFF
-      box-shadow 0px 0px 2.5em #263CFF, 0px 0px 8em #2D74FF
-    &__planet
-      position absolute
-      width 126%
-      padding-bottom 126%
-      left -13%
-      top 42%
-      border-radius 50%
-      background radial-gradient(84.49% 66.22% at 50% 59.94%, #000000 5.73%, #030419 71.48%, #0E1247 81.25%, #36299B 91.15%)
-      box-shadow 0px -4.5em 4em rgba(28, 0, 238, 0.17), 0px -0.625em 0.5em rgba(92, 70, 255, 0.11), inset 0px 3.5em 3.5em rgba(48, 44, 245, 0.17), inset 0px 0.375em 0.75em rgba(104, 67, 252, 0.23)
-
-.hero-bottom
-  margin-top 15%
-  .hero-btn
-    margin 3.5rem auto
-    width 100%
-    max-width 17.5rem
-    height 4rem
-    background #4251FA linear-gradient(92.99deg, #8D0EC0 0%, #4251FA 100%)
-    box-shadow 0px 10px 22px rgba(65, 81, 250, 0.21)
-    font-weight 700
-    font-size 1.1875rem
-    line-height 1.263
-    letter-spacing -0.005em
-    position relative
-    transition transform .2s ease-out, background-color .2s ease-out
-    &::after
-      content ""
-      position absolute
-      top 0
-      right 0
-      left 0
-      bottom 0
-      border-radius inherit
-      background linear-gradient(92.99deg, rgba(255,255,255,0.15) 0%, rgba(255,255,255,0) 100%)
-      box-shadow 0px 15px 33px rgba(65, 81, 250, 0.25)
-      opacity 0
-      transition opacity .2s ease-out
-    &:hover,
-    &:focus
-      transform translateY(-2px)
-      &::after
-        opacity 1
-    &:active
-      transform none
-      transition-duration 0s
-      &::after
-        transition-duration 0s
-        opacity 0
-
-@keyframes gate
-  from
-    transform translate3d(0,0,0) rotate(0deg)
-  to
-    transform translate3d(0,0,0) rotate(360deg)
 
 .section-intro
   position relative
@@ -2345,10 +2110,6 @@ td
         grid-column 8 / span 4
 
 @media screen and (max-width: 1279px)
-  .section
-    padding-top 6rem
-    padding-bottom 6rem
-
   .section-features
     .features-graphics
       font-size 1.25rem
@@ -2356,20 +2117,6 @@ td
       grid-column 1/span 12
 
 @media screen and (max-width: 1024px)
-  .section
-    padding-top 4rem
-    padding-bottom 4rem
-
-  .section-hero
-    display grid
-    .container
-      .headings
-        .subtitle
-          font-size 1.75rem
-          line-height 1.571
-          letter-spacing -0.015em
-          margin-top 2rem
-
   .section .section-header
       grid-column 1 / span 12
 
@@ -2513,10 +2260,6 @@ td
         padding-left 1.5rem
         padding-right 1.5rem
 
-  .section-hero
-    .hero-graphics
-      font-size 0.5rem
-
   .section-intro
     .section-graphics
       font-size 0.75rem
@@ -2564,38 +2307,6 @@ td
       font-size 1rem
       line-height 1.375
       margin-top 1.5rem
-
-  .nav-primary
-    .logo
-      height 1.25rem
-    a
-      padding 0.75rem
-      .text
-        display none
-      .icon
-        display block
-
-  .section-hero
-    min-height 32rem
-    max-height 42rem
-    margin-bottom 3rem
-    .section-container
-      padding-top 0.75rem
-      padding-bottom 2rem
-    .container
-      .nav-primary
-        margin-bottom 3rem
-      .headings
-        .title
-          padding-left 1rem
-          padding-right 1rem
-        .subtitle
-          font-size 1.1875rem
-          line-height 1.579
-          letter-spacing -0.005em
-          margin-top 1.5rem
-    .hero-graphics
-      font-size 0.36rem
 
   .section-nav
     .nav
@@ -2830,14 +2541,4 @@ td
 
   .section-community .cards__item
     padding 1.5rem
-
-@media screen and (min-width: 1536px)
-  .section-hero
-    .hero-graphics
-      overflow visible
-
-@media screen and (min-width: 1920px)
-  .section
-    padding-top 8rem
-    padding-bottom 8rem
 </style>
