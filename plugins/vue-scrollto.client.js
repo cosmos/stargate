cded46cb7b5d9aa306e136790d079c2a7d3e318f
Cyrus Goh <hello@lovincyrus.com>
2021-01-05 16:27:08 -0800

    comment out vue-scrollto

--- plugins/vue-scrollto.client.js
+++ plugins/vue-scrollto.client.js
@@ -1,18 +1,18 @@
-import Vue from 'vue'
-import VueScrollTo from 'vue-scrollto'
+// import Vue from 'vue'
+// import VueScrollTo from 'vue-scrollto'
 
-// TODO: config scrolling speed
-// https://vue-scrollto.netlify.app/docs/#options
-Vue.use(VueScrollTo, {
-  container: 'body',
-  duration: 500,
-  easing: 'ease',
-  offset: 0,
-  force: true,
-  cancelable: true,
-  onStart: false,
-  onDone: false,
-  onCancel: false,
-  x: false,
-  y: true,
-})
+// // Uncomment to use
+// // https://vue-scrollto.netlify.app/docs/#options
+// Vue.use(VueScrollTo, {
+//   container: 'body',
+//   duration: 500,
+//   easing: 'ease',
+//   offset: 0,
+//   force: true,
+//   cancelable: true,
+//   onStart: false,
+//   onDone: false,
+//   onCancel: false,
+//   x: false,
+//   y: true,
+// })
