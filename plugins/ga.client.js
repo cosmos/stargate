485403dfcf02b5ad6ead299e9302be1f8fb8ba4a
tk9653052 <126038092+tk9653052@users.noreply.github.com>
2023-02-24 21:56:16 +0530

    Update ga.client.js

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -2,12 +2,9 @@
 
 export default ({ app }) => {
   /*
-  ** Only run on client-side and only in production mode
+  ** client-side and only in production mode
   */
   if (process.env.NODE_ENV !== 'production') return
-  /*
-  ** Include Google Analytics Script
-  */
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
